a315ab329ebfff07c5191d17a7ea192f415ffbf0
Faiqah Balqis <157272082+faiqahbalqis@users.noreply.github.com>
2024-01-21 17:53:59 +0800

    Update telegrambot.ino

--- telegrambot/telegrambot.ino
+++ telegrambot/telegrambot.ino
@@ -1,9 +1,9 @@
 #include "CTBot.h"
 CTBot myBot;
 
-String ssid = "yesaya";
-String pass = "faiqahbalqis";
-String token = "5472570344:AAGVGt1t5mV3-litGeZ46X4n_3uqaB2l9y4";
+String ssid = "*****";
+String pass = "*****";
+String token = "**********";
 const int id = 407617129;
 
 void setup() {
